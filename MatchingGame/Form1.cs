88861f288ea2a44286662f3351d91db4f87ee68f
michaelekwandja <145231541+michaelekwandja@users.noreply.github.com>
2024-08-13 17:04:59 +0200

    Add reference variables and a timer control to your matching game WinForms app

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,6 +12,15 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        // firstClicked points to the first Label control 
+        // that the player clicks, but it will be null 
+        // if the player hasn't clicked a label yet
+        Label firstClicked = null;
+
+        // secondClicked points to the second Label control 
+        // that the player clicks
+        Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -69,7 +78,17 @@ namespace MatchingGame
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                clickedLabel.ForeColor = Color.Black;
+                // If firstClicked is null, this is the first icon 
+                // in the pair that the player clicked,
+                // so set firstClicked to the label that the player 
+                // clicked, change its color to black, and return
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
             }   
         }
     }
